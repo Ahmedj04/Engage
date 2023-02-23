2fff73cd9d30eec8ad9290fdd2a4230ce7599ab3
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-02-23 18:09:08 +0530

    select all for delete in season-underprocess

--- pages/places/place.js
+++ pages/places/place.js
@@ -21,6 +21,7 @@ import searchFunction from '../../components/searchFunction';
 let colorToggle;
 let language;
 let currentProperty;
+let check = [];
 let currentLogged;
 // main function
 const Place = () => {
@@ -173,7 +174,9 @@ const Place = () => {
             setCategories(response?.data?.place_category)
             //setLanguages(response?.data?.place_languages)
             setLanguages((response?.data?.place_languages?.map(lang => GlobalData.LanguageData.filter(i => i.language_code === lang.language))).flat())
-            setSeasons(response?.data?.place_seasons)
+            let tempSeason=[]
+            response?.data?.place_seasons.map((season, id) => { tempSeason.push({ ...season,'isChecked': false }) })
+            setSeasons(tempSeason)
             let images = []
             response?.data?.images.map((image, id) => { images.push({ ...image, 'image_idx': id, 'isChecked': false }) })
             setPlaceImage(images)
@@ -184,8 +187,34 @@ const Place = () => {
         console.log("Place Data fetched");
     }
 
+    //handle check box
+    const handlecheckbox = (e,edit) => {
+        const { name, checked } = e.target;
+        let tempCon = seasons.map((item) =>
+          item.season_id === name ? { ...item, isChecked: checked } : item
+        );
+        setSeasons(tempCon); 
+        check = tempCon
+          .filter((i) => i.isChecked === true)
+          .map((j) => {
+            return j.image_id;
+          });
+      };
 
+    const allCheckbox = (e) =>{
+      //  const { name, checked } = e.target;
+        // let tempCon = seasons.map((item) =>
+        //   item.season_id === name ? { ...item, isChecked: checked } : item
+        // );
 
+      let tempCon=[];
+     seasons.map((item) =>
+     tempCon.push({...item,'isChecked':'checked'})
+        );
+
+        setSeasons(tempCon); 
+        check = tempCon
+    }
     //changing multiselected data
     const languageViews = (viewData) => {
         console.log("multiselect data changed")
@@ -199,6 +228,13 @@ const Place = () => {
         // });
         // setFinalView(final_view_data);
         // setRoomView(1)
+    }
+
+    //delete All seasons
+    function deleteAllSeason(){
+        alert(JSON.stringify(seasons));
+        let remainingSeasons =seasons.filter(season=>season.isChecked != 'checked');
+        setSeasons(remainingSeasons);
     }
 
     //catgory
@@ -581,7 +617,7 @@ const Place = () => {
                                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"></path></svg>
                                     </span>
 
-                                    <button onClick={() => { alert("all deleted") }} data-tooltip="Delete" aria-label="Delete" className={`${color?.textgray} hover:${color?.text} cursor-pointer p-1 ${color?.hover} rounded inline-flex justify-center`}>
+                                    <button onClick={() => { deleteAllSeason() }} data-tooltip="Delete" aria-label="Delete" className={`${color?.textgray} hover:${color?.text} cursor-pointer p-1 ${color?.hover} rounded inline-flex justify-center`}>
                                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
                                     </button>
 
@@ -610,7 +646,10 @@ const Place = () => {
                                                     <th scope="col" className="p-4">
                                                         <div className="flex items-center">
                                                             <input id="checkbox-all" aria-describedby="checkbox-1" type="checkbox"
-                                                                name="allSelect"
+                                                                name="allSelect" 
+                                                                onChange={(e) => {
+                                                                    allCheckbox(e);
+                                                                  }}
                                                                 className="bg-gray-50 border-gray-300 text-cyan-600  focus:ring-3 focus:ring-cyan-200 h-4 w-4 rounded" />
                                                             <label htmlFor="checkbox-all" className="sr-only">checkbox</label>
                                                         </div>
@@ -634,11 +673,23 @@ const Place = () => {
                                             <tbody className={` ${color?.whitebackground} divide-y  divide-gray-200`}>
                                                 {seasons.map((season, index) => {
                                                     return (<>
-                                                        {(editRow?.edit === 1 && editRow.id === index) ? <tr key={index}>
+                                                        {(editRow?.edit === 1 && editRow.id === index) ? 
+                                                        <tr key={index}>
+                                                            {JSON.stringify(editSeason)}
                                                             <td className="p-4 w-4">
                                                                 <span className="flex items-center">
-                                                                    <input id="checkbox-1" name={season?.index} aria-describedby="checkbox-1" type="checkbox"
-                                                                        className="bg-gray-50 border-gray-300 text-cyan-600  focus:ring-3 focus:ring-cyan-200 h-4 w-4 rounded" />
+                                                                    <input
+                                                                     type="checkbox"
+                                                                     id={editSeason?.season_id}
+                                                                     tooltip
+                                                                     title="Click here to delete image."
+                                                                     name={editSeason?.season_id}
+                                                                     checked={editSeason.isChecked || false}
+                                                                     onChange={(e) => {
+                                                                       handlecheckbox(e);
+                                                                     }}
+                                                                aria-describedby="checkbox-1"
+                                                                className="bg-gray-50 border-gray-300 text-cyan-600  focus:ring-3 focus:ring-cyan-200 h-4 w-4 rounded" />
                                                                     <label htmlFor="checkbox-1" className="sr-only">checkbox</label>
                                                                 </span>
                                                             </td>
@@ -699,7 +750,17 @@ const Place = () => {
                                                             <tr key={index}>
                                                                 <td className="p-4 w-4">
                                                                     <span className="flex items-center">
-                                                                        <input id="checkbox-1" name={season?.index} aria-describedby="checkbox-1" type="checkbox"
+                                                                        <input 
+                                                                         type="checkbox"
+                                                                         id={season?.season_id}
+                                                                         tooltip
+                                                                         title="Click here to delete image."
+                                                                         name={season?.season_id}
+                                                                         checked={season.isChecked || false}
+                                                                         onChange={(e) => {
+                                                                           handlecheckbox(e);
+                                                                         }}
+                                                                        aria-describedby="checkbox-1"
                                                                             className="bg-gray-50 border-gray-300 text-cyan-600  focus:ring-3 focus:ring-cyan-200 h-4 w-4 rounded" />
                                                                         <label htmlFor="checkbox-1" className="sr-only">checkbox</label>
                                                                     </span>
