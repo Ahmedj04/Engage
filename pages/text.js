0c85b8ec7c8c94163df28e2c354041a3996ae074
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-05-22 17:28:55 +0530

    Update text.js

--- pages/text.js
+++ pages/text.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+// import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 const RoomPriceCalendar = () => {
     const [events, setEvents] = useState([])
